9f6fe7c630d5b5db88f3fa90b2c1d5ab73695323
RonSala <ron.sala@gmail.com>
2019-08-04 02:44:24 -0400

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  get 'memories/index'
+  get 'memories/new'
+  get 'memories/create'
+  get 'memories/show'
+  get 'memories/edit'
+  get 'memories/update'
+  get 'memories/destroy'
+  get 'events/index'
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
+  get 'events/edit'
+  get 'events/update'
+  get 'events/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, shallow: true do
