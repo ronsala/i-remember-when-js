19d2378d722a42833f5a4bfbe9b936b1e224e715
RonSala <ron.sala@gmail.com>
2019-08-18 03:15:57 -0400

    pass Event has many memories

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,27 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  let (:user) {
-    User.create(
-      username: "Eve",
-      email: "eve@example.com",
-      password: "123", 
-      bio: "In search of truth."
-    )
-  }
+  before (:each) do
 
-  let (:event) {
-    Event.create(
-      name: "The Big Event",
-      country: "United States",
-      day: 1,
-      month: 5,
-      year: 1985,
-      description: "It was that big."
-    )
-  }
+    @event = Event.create(
+        name: "The Big Event",
+        country: "United States",
+        day: 1,
+        month: 5,
+        year: 1985,
+        description: "It was that big."
+      )
+
+    @user1 = User.create(
+        username: "Lexi",
+        email: "lexi@example.com",
+        password: "123", 
+        bio: "In search of truth."
+      )
+
+     @user2 = User.create(
+        username: "Eve",
+        email: "eve@example.com",
+        password: "123", 
+        bio: "In search of experience."
+      )
+    
+
+     @memory1 = Memory.create(
+        event_id: @event.id,
+        user_id: @user1.id,
+        title: "My Earliest Memory", 
+        body: "It was a dark and stormy night. Out stepped the captain...."
+      )
+
+     @memory2 = Memory.create(
+        event_id: @event.id,
+        user_id: @user2.id,
+        title: "Life Changing", 
+        body: "Best speech ever!"
+      )
+  end
 
   it "is valid with name, country, day, month, year, description" do
-    expect(event).to be_valid
+    expect(@event).to be_valid
+  end
+
+  it "has many memories" do
+    expect(@event.memories.first).to eq(@memory1)
+    expect(@event.memories.last).to eq(@memory2)
   end
 end
