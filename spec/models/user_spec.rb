79ee1935bd43250d613cd12be43c2a369af53262
RonSala <ron.sala@gmail.com>
2019-08-18 03:19:24 -0400

    clean up user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   before :each do
-    # DatabaseCleaner.clean
-  
-    # 10.times do
-    #   user = User.new(username: Faker::Internet.username, email: Faker::Internet.safe_email, password: Faker::Internet.password, bio: Faker::Lorem.paragraph)
-    #   # binding.pry
-    #   user.save
-    # end
-
+    
     10.times do
       user = User.new
       user[:username] = Faker::Internet.username
@@ -19,7 +12,6 @@ RSpec.describe User, type: :model do
   end
 
   it "creates users" do
-    # binding.pry
     expect(User.all.count).to eq(10)
   end
 end
