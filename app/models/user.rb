baf36a2ffeb0a2bd6cee95a6353ac0fd485b716f
RonSala <ron.sala@gmail.com>
2019-08-18 04:29:04 -0400

    pass User is not valid without password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   has_many :memories
   has_many :events, through: :memories
 
-  attr_accessor :username, :email, :password, :password_confirmation, :bio
-  # has_secure_password
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  # it "creates users" do
-  #   10.times do
-  #     user = User.new
-  #     user[:username] = Faker::Internet.username
-  #     user.save
-  #   end
-  #   expect(User.all.count).to eq(10)
-  # end
-
   let (:user) {
-    User.create(
+    User.create!(
       username: "Mark",
       email: "mark@example.com",
       password: "123",
@@ -20,8 +11,22 @@ RSpec.describe User, type: :model do
     )
   }
 
+  # let (:admin) {
+  #   User.create(
+  #     username: "Mandy",
+  #     email: "mandy@example.com",
+  #     password: "123",
+  #     bio: "I rule!.",
+  #     admin: true
+  #   )
+  # }
+
   it "is valid with username, email, password, bio" do
     expect(user).to be_valid
+  end
+
+  it "is not valid without password" do
+    expect(User.new(username: "MyName")).not_to be_valid
   end
 
   # it "creates an admin" do
