af33ae3ddb76d9710f1e131e0acc74bfaca93119
RonSala <ron.sala@gmail.com>
2019-08-18 02:07:32 -0400

    pass Event is valid with name, country, day, month, year, description

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  let (:user) {
+    User.create(
+      username: "Eve",
+      email: "eve@example.com",
+      password: "123", 
+      bio: "In search of truth."
+    )
+  }
 
+  let (:event) {
+    Event.create(
+      name: "The Big Event",
+      country: "United States",
+      day: 1,
+      month: 5,
+      year: 1985,
+      description: "It was that big."
+    )
+  }
+
+  it "is valid with name, country, day, month, year, description" do
+    expect(event).to be_valid
+  end
 end
