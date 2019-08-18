36709010e13fc0bf9cd89ff62cbf4ea34222291e
RonSala <ron.sala@gmail.com>
2019-08-18 03:48:17 -0400

    replace 'User creates users' with 'User is valid with username, email, password, bio', pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  before :each do
-    
-    10.times do
-      user = User.new
-      user[:username] = Faker::Internet.username
-      user.save
-    end
+  # it "creates users" do
+  #   10.times do
+  #     user = User.new
+  #     user[:username] = Faker::Internet.username
+  #     user.save
+  #   end
+  #   expect(User.all.count).to eq(10)
+  # end
+
+  let (:user) {
+    User.create(
+      username: "Mark",
+      email: "mark@example.com",
+      password: "123",
+      bio: "Out of the wild in style."
+    )
+  }
+
+  it "is valid with username, email, password, bio" do
+    expect(user).to be_valid
   end
 
-  it "creates users" do
-    expect(User.all.count).to eq(10)
-  end
+  # it "creates an admin" do
+  #   admin = User.create(username: "Mandy", admin: true)
+
+  # end
 end
