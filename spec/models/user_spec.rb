fb589c03781146428cf82097a7334dc72f3bc461
RonSala <ron.sala@gmail.com>
2019-08-18 04:35:57 -0400

    pass User is valid with an admin boolean

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,15 +11,15 @@ RSpec.describe User, type: :model do
     )
   }
 
-  # let (:admin) {
-  #   User.create(
-  #     username: "Mandy",
-  #     email: "mandy@example.com",
-  #     password: "123",
-  #     bio: "I rule!.",
-  #     admin: true
-  #   )
-  # }
+  let (:admin) {
+    User.create!(
+      username: "Mandy",
+      email: "mandy@example.com",
+      password: "123",
+      bio: "I rule!.",
+      admin: true
+    )
+  }
 
   it "is valid with username, email, password, bio" do
     expect(user).to be_valid
@@ -29,8 +29,7 @@ RSpec.describe User, type: :model do
     expect(User.new(username: "MyName")).not_to be_valid
   end
 
-  # it "creates an admin" do
-  #   admin = User.create(username: "Mandy", admin: true)
-
-  # end
+  it "is valid with an admin boolean" do
+    expect(admin).to be_valid
+  end
 end
