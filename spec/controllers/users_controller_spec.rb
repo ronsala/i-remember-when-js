c0292ec317477f3548e91ad417dd89d8c956d8fe
RonSala <ron.sala@gmail.com>
2019-08-19 03:07:53 -0400

    pass WelcomeController GET #home returns http success

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+
+  DatabaseCleaner.clean
+
   describe "GET index" do
     it "assigns @users" do
-      user = User.create
+      user = User.create!(password: "123")
       get :index
       expect(assigns(:users)).to eq([user])
     end
