4e9f5ffbdab5852097b8dd2b8587946b6e1aa61b
RonSala <ron.sala@gmail.com>
2019-08-19 05:13:05 -0400

    pass UsersController GET index renders the index template

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe UsersController, type: :controller do
       get :index
       expect(assigns(:users)).to eq([user])
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
 end
