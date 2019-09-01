5bb4f82bbf7ee598aa62a42605124b5368ca2b8c
RonSala <ron.sala@gmail.com>
2019-09-01 09:37:34 -0400

    add 'most_recent' scope to Memory

---
+++ db/migrate/20190901132437_add_time_stamps_to_memory.rb
@@ -0,0 +1,8 @@
+class AddTimeStampsToMemory < ActiveRecord::Migration[5.2]
+  def change
+    add_timestamps :memories, null: true 
+  
+    change_column_null :memories, :created_at, false
+    change_column_null :memories, :updated_at, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_085148) do
+ActiveRecord::Schema.define(version: 2019_09_01_132437) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_08_18_085148) do
     t.integer "user_id"
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
