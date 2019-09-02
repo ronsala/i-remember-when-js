603e7e454e407e34dc308577d2e0151c2a886834
RonSala <ron.sala@gmail.com>
2019-09-02 02:05:26 -0400

    uncomment User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :memories
   has_many :events, through: :memories
 
-  # validates :username, presence: true, uniqueness: :true
-  # validates :email, uniqueness: :true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  # validates :password, confirmation: true, length: { in: 8..20 }
+  validates :username, presence: true, uniqueness: :true
+  validates :email, uniqueness: :true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :password, confirmation: true, length: { in: 8..20 }
 end
