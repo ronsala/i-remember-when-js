961fd01397756d50f426cd1674ba65f72534d194
RonSala <ron.sala@gmail.com>
2019-09-05 01:27:29 -0400

    write Account Creating account shows registration page

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Creating account", type: :feature do
-# Rspec.describe "Creating account", type: :feature do
-  
+RSpec.describe 'Creating account', type: :feature do
+  it 'shows registration page' do
+    visit root_path
+    click_button('Sign up')
+    expect(page).to have_text 'Sign Up'
+  end
 end
