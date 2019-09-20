dcf1e59f0df468f063ff5e683cba9c5322838cfd
RonSala <ron.sala@gmail.com>
2019-09-20 02:27:24 -0400

    do initial facebook omniauth setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
   has_many :memories
   has_many :events, through: :memories
 
