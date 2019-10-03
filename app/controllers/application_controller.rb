d7228fd301a2c33ebd1ff4c316c160decb3cb5e2
RonSala <ron.sala@gmail.com>
2019-10-03 05:29:19 -0400

    update application_controller, memories/nes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def current_user
+    if logged_in?
+      User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
   protected
 
   def configure_permitted_parameters
