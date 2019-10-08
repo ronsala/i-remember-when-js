ed25363c3432d5f8edb5694cc84a7a8c0a6c4f17
RonSala <ron.sala@gmail.com>
2019-10-08 19:10:13 -0400

    update sessions_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,21 +4,27 @@
 class Users::SessionsController < Devise::SessionsController
   # GET /users/sign_in
   def new
-    render '/users/sign_in'
+    @user = User.new
   end
 
   # POST /account/sign_in
   def create
-    @user = User.find_by(sign_in_params)
+    @user = User.find_by(email: sign_in_params[:email])
     session[:current_user_id] = @user.id
     redirect_to @user
+  end
+
+  # DELETE account/sign_out
+  def destroy
+    session.clear
+    redirect_to '/'
   end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username)
+      user_params.permit(:email, :password, :remember_me)
     end
   end
 end
