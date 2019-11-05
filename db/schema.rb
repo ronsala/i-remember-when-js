1f1e689ad9f436301806597da8f0008bda007205
RonSala <ron.sala@gmail.com>
2019-11-05 04:47:09 -0500

    debug 'RecordNotUnique' exception when running rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_053237) do
+ActiveRecord::Schema.define(version: 2019_11_05_093704) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2019_09_20_053237) do
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index [nil], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
