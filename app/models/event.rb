b228b08343dcf06189398e69b73ea4572b27a08f
RonSala <ron.sala@gmail.com>
2019-12-09 01:59:07 -0500

    add user_id to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  belongs_to :creator, foreign_key: 'user_id',class_name: "User"
+  belongs_to :user
   has_many :memories
   has_many :users, through: :memories
   # TODO:
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_042755) do
+ActiveRecord::Schema.define(version: 2019_12_09_065416) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "country"
     t.string "description"
     t.date "date"
+    t.integer "user_id"
   end
 
   create_table "memories", force: :cascade do |t|
