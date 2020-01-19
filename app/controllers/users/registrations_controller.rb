72aeb88f8b6c0850e79e5944b71b3257bf9fa436
RonSala <ron.sala@gmail.com>
2020-01-19 09:21:18 -0500

    mv devise strong params to application controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,8 @@
 
 # For user account management.
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
   before_action :authenticate_user!, only: %i[edit update destroy]
 
 
@@ -50,18 +50,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:username, :email, :password, :password_confirmation, :bio)
-    end
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  end
 
   # The path used after sign up.
   def after_sign_up_path_for(user)
