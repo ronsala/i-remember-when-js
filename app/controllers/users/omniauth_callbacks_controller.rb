4ab71d013b4b9349dade64606cb669780b937873
RonSala <ron.sala@gmail.com>
2020-01-19 05:20:41 -0500

    fix devise signin on signup with google

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,9 +4,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
+      if @user.persisted?
       session[:user_id] = @user.id
       set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+      sign_in(:user, @user)
       redirect_to @user
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
