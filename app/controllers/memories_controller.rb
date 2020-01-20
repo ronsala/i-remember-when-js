5e7ff519f11e985f790ed10118531fccfa62a0ef
RonSala <ron.sala@gmail.com>
2020-01-20 03:38:06 -0500

    add flash messages, fix memories controller

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class MemoriesController < ApplicationController
-  before_action :set_event, except: %i[index show edit update]
   before_action :set_memory, only: %i[show edit update destroy]
 
   def index
@@ -10,50 +9,50 @@ class MemoriesController < ApplicationController
   end
 
   def new
+    @event = Event.find(params[:event_id])
     @memory = @event.memories.build
   end
 
   def create
+    @event = Event.find(params[:event_id])
     @memory = @event.memories.build(memory_params)
     @memory.user_id = current_user.id
 
     if @memory.save
+      flash[:notice] = "#{@memory.title} created!"
       redirect_to event_memory_path(@event, @memory)
     else
-      render :new
+      flash[:error] = @memory.errors.full_messages.join(' | ')
+      redirect_to new_event_memory_path(@event)
     end
   end
 
   def show
-    @memory = Memory.find(params[:id])
     render :show
   end
 
-  def edit;end
+  def edit; end
 
   def update
     if @memory.update(memory_params)
+      flash[:notice] = "#{@memory.title} updated!"
       redirect_to memory_path(@memory)
     else
-      render :edit
+      flash[:error] = @memory.errors.full_messages.join(' | ')
+      redirect_to edit_event_memory_path(@memory.event)
     end
   end
 
   def destroy
-    @memory = Memory.find(params[:id])
-    event_id = @memory.event_id
-    memory.destroy
-    redirect_to "/events/<%= event_id %>"
+    flash[:notice] = "#{@memory.title} deleted!"
+    @memory.destroy
+    redirect_to event_path(@memory.event)
   end
 
   protected
 
   def memory_params
-    params.require(:memory).permit(:title, :body)
-  end
-
-  def set_event
-    @event = Event.find(params[:event_id])
+    params.require(:memory).permit(:title, :body, :event_id)
   end
 
   def set_memory
