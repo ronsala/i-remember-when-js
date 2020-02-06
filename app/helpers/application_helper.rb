cf32a3ac4dc0d40a9c5311af080514ad8adfcb79
RonSala <ron.sala@gmail.com>
2020-02-06 08:52:36 -0500

    misc styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,16 @@
 
 # Changes dates to Month, Day, Year format.
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
+
   def format_date(date)
     date.strftime('%B %e, %Y')
   end
