2ae79341984f7a2754585adcc9b72fc3860c8cb3
RonSala <ron.sala@gmail.com>
2020-04-26 16:54:33 -0400

    add attributes and associations to serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :country, :description, :date, :user_id, :created_at, :updated_at
+  has_many :memories, dependent: :destroy
+  has_many :users, through: :memories
 end
--- app/serializers/memory_serializer.rb
+++ app/serializers/memory_serializer.rb
@@ -1,3 +1,5 @@
 class MemorySerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :event_id, :user_id, :title, :body, :created_at, :updated_at
+  belongs_to :user
+  belongs_to :event
 end
